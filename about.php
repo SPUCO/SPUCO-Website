1ca170caad2d21d3d8a06bc0325ed8a9c806dafd
SPUCO <160882656+SPUCO@users.noreply.github.com>
2024-02-25 20:50:48 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php
+
+include('layouts/header.php');
+
+?>
+  </div>
+</div>
+
+<!--------- About page ------------>
+<section class="my-5 py-5">
+	<div class="container text-center mt-3 pt-5">
+		<h2 class="form-weight-bold">About Us</h2>
+		<hr class="max-auto">
+	</div>
+	<div class="small-container">
+    <div class="row" style="margin: 20px; padding: 20px; font-family: verdana; font-size: large;">
+      <h1></h1>
+      <p style="font-family: verdana; font-size: 16px"></p>
+      <div><br><img src="assets/images/spuco images/spuco_logo.jpg" style="width: 200px; height: 200px">
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<?php
+
+include('layouts/footer.php');
+	
+?>
---
+++ index.php
@@ -0,0 +1,185 @@
+<?php
+include('layouts/header.php');
+?>
+  </div>
+</div>
+<!------------- Website Messages----------->
+<p style="color: red; font-weight: bold; text-align: center" class="text-center"><?php if(isset($_GET['error'])){ echo $_GET['error']; }?></p>
+<p style="color: green" class="text-center"><?php if(isset($_GET['message'])){ echo $_GET['message']; }?></p>
+<!------------- Welcome Body ----------->
+<div class="offer0">
+	<div class="container">
+		<div class="row">
+		<!-- Below Header Intro or Banner --> 
+
+		<div class="col-2" style="background-image: url('assets/images/background pics/pic1.jpg');">
+			<h1>Welcome To SPU CODERS</h1>
+			<p style="font-weight: bold">Shaping the future with technology</p>
+			<a href="<?php echo "about.php"; ?>" class="btn" style="background-color: green">Learn More &#8594;</a>
+		</div><br>
+		
+		<!----------- Programming Languages ---------->
+		<div class="programming_languages">
+		<h3 class="title">PROGRAMMING LANGUAGES</h3>
+			<div class="small-container">
+				<div class="row">
+					<div class="col-5">
+						<img src="assets/images/programming pics/html_pic.png" alt="Snow">
+					</div>
+					<div class="col-5">
+						<img src="assets/images/programming pics/css_pic.png" alt="Snow">
+					</div>
+					<div class="col-5">
+						<img src="assets/images/programming pics/javascript_pic.jpg" alt="Snow">
+					</div>
+					<div class="col-5">
+						<img src="assets/images/programming pics/php_pic.png" alt="Snow">
+					</div>
+					<div class="col-5">
+						<img src="assets/images/programming pics/sql_pic.png" alt="Snow">
+					</div>
+					<div class="col-5">
+						<img src="assets/images/programming pics/python_pic.png" alt="Snow">
+					</div>
+					<div class="col-5">
+						<img src="assets/images/programming pics/java_pic.png" alt="Snow">
+					</div>
+					<div class="col-5">
+						<img src="assets/images/programming pics/c++_pic.jpg" alt="Snow">
+					</div>
+					<div class="col-5">
+						<img src="assets/images/programming pics/csharp_pic.png" alt="Snow">
+					</div>
+					<div class="col-5">
+						<img src="assets/images/programming pics/github_pic.png" alt="Snow">
+					</div>
+				</div>
+			</div>
+		</div>
+	  </div>
+	</div>
+</div>
+
+<!------- Slideshow ----------->
+<div class="categories">
+   <h3 class="title">SPU CODERS</h3>
+	<div class="small-container">
+		<div class="row">
+			<div class="col-3">
+				<img src="assets/images/gallery/coding_pics1.jpeg" alt="Snow" style="border-radius: 11%">
+			</div>
+			<div class="col-3">
+				<img src="assets/images/gallery/coding_pics2.jpg" alt="Snow" style="border-radius: 11%">
+			</div>
+			<div class="col-3">
+				<img src="assets/images/gallery/coding_pics3.jpg" alt="Snow" style="border-radius: 11%">
+			</div>
+		</div>
+	</div>
+</div>
+
+<!------- Club Details ----------->
+<div class="small-container">
+    <h3 class="title">PURPOSE</h3>
+	<div class="row">
+        <p>The SPUCO Is Established As An Organization That Aims To Foster, Aid, Inspire And Motivate Students To Broaden Their Skill And Knowledge In Coding. The University Main Focus Is Theory Aspect Of Programming, The SPUCO Is A Platform That Aims To Improve And Focus On The Technical Aspect Of Programming, Which Is Coding. In Achieving This The Club Aims To Organize And Promote Social Activities For Students Specifically Coding Competitions.<br><br></p>
+        <p style="color: red">Only 100 First Year Members Will Be Accepted.</p>
+    </div>
+    <div class="row">
+        <h3 class="title">STRUCTURE</h3>
+		<p>We, The Sol Plaatje University Coding Organization (SPUCO), And Its Structures: Respect And Subscribe To The Norms And Values Associated With A Democratic Society In Particular The Values Stated In Chapter 1 (Founding Provisions) And 2 (Bill of Rights) Of The Constitution Of South Africa ,1996, As Amended. Embrace The SRC’s Vision, Mission, And Strategic Objectives. Commit To Serve The SPU Students Under All The Coding Courses By Enhancing The Interests Of Students With Due Observance Of The Laws Of South Africa, The Rules Of The University, The Constitution For Student Governance And The Codes Of Conduct Of The University, The SRC And The SPUCO. Strive To Promote Coding In The University. Aspire To Shape Coding In South Africa As A Whole And Not Only In The Sol Plaatje University, Embrace The Principles Of Tolarance And Accountability. We, the Sol Plaatje University Coding Organization: Recognize the Statue Of Sol Plaatje University Kimberley ,2017, Policies Of The University, General Rules Of The University, Senate Rules For Faculties/Schools Of The University, Senates Standing Order Of The University, School/Faulty Standing Orders, School (Programme) Standing Orders.
+        </p>
+	</div>
+</div>
+
+<!------- Slideshow ----------->
+<div class="categories">
+   <h3 class="title">SPU CODERS</h3>
+	<div class="small-container">
+		<div class="row">
+			<div class="col-3">
+				<img src="assets/images/executive pics/President-Co Founder ( Awande Ayanda Gumbi) BSc Biological Sciences.jpg" alt="Snow">
+				<p>President & Co-Founder</p>
+			</div>
+			<div class="col-3">
+				<img src="assets/images/executive pics/Deputy President( Kay Mudau) BscHons Computer Science.jpg" alt="Snow">
+				<p>Deputy President</p>
+			</div>
+			<div class="col-3">
+				<img src="assets/images/executive pics/WebSoftware Developer(Zwelibanzi Langeni) Bsc Mathematical and Computer Science.jpg" alt="Snow">
+				<p>Web / Software Developer</p>
+			</div>
+			<div class="col-3">
+				<img src="assets/images/executive pics/Secretary General ( Rethabile Binang) ICT.jpg" alt="Snow">
+				<p>Secretary General</p>
+			</div>
+			<div class="col-3">
+				<img src="assets/images/executive pics/Legal, Policy and Constitutional Affairs Officer ( Koketso Jampane ) ICT.jpg" alt="Snow">
+				<p>Legal, Policy and Constitutional Affairs Officer</p>
+			</div>
+			<div class="col-3">
+				<img src="assets/images/executive pics/Mentorship Coordinator & Outreach Coordinator ( Vanessa Skosana) ICT.jpg" alt="Snow">
+				<p>Mentorship Coordinator & Outreach Coordinator</p>
+			</div>
+			<div class="col-3">
+				<img src="assets/images/executive pics/Marketing Coordinator(Gadifele Serame) ICT.jpg" alt="Snow">
+				<p>Marketing Coordinator</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!---------- testimonials --------->
+<div class="testimonials">
+	<div class="small-container"><br><br>
+	  <h3 class="title">TESTIMONIALS</h3>
+		<h3 class="titledescription">Help Us Improve By Mentioning Problems & Challenges Experienced On Our Website. Any Response Is Appreciated.
+		<div class="row">
+			<!---import the files--->
+			<?php include('server/gettestimonials.php'); ?>
+			<?php while($row = $testimonials->fetch_assoc()) { ?>
+			<div class="col-3">
+				<i class="fa fa-quote-left"></i>
+				<p><?php echo $row['fldtestimonialscomment']; ?></p>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star-o"></i>
+				</div>
+				<img src="<?php if(isset($row['fldtestimonialsimage'])){ echo "assets/images/".$row['fldtestimonialsimage']; }else{ echo "assets/images/unknownimage.png"; } ?>" alt="Snow">
+				<h3><?php echo $row['fldtestimonialsemail']; ?></h3>
+			</div>
+			<?php } ?>
+		</div>
+	</div>
+</div>
+
+<!----------- Coding Resources ---------->
+<div class="coding_tools">
+    <h3 class="title">CODING RESOURCES</h3>
+    <div class="row">
+		<div class="col-5">
+			<img src="assets/images/partners pics/google_pic.png" alt="Snow">
+		</div>
+		<div class="col-5">
+			<img src="assets/images/partners pics/microsoft_pic.png" alt="Snow">
+		</div>
+		<div class="col-5">
+			<img src="assets/images/partners pics/ibm_pic.jpg" alt="Snow">
+		</div>
+		<div class="col-5">
+			<img src="assets/images/partners pics/cisco_pic.png" alt="Snow">
+		</div>
+		<div class="col-5">
+			<img src="assets/images/partners pics/moodle_pic.png" alt="Snow">
+		</div>
+	</div>
+</div>
+
+<?php
+
+include('layouts/footer.php');
+	
+?>
