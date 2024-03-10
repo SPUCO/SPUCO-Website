75d4e94a99dab8e7e1fd2d6f605bd4130ac69794
SPUCO <160882656+SPUCO@users.noreply.github.com>
2024-03-10 17:35:19 +0300

    Add files via upload

--- server/connection.php
+++ server/connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '','spucodatabase')
+$conn = mysqli_connect('localhost', 'root', '','spucoders_database')
         or die("couldn't connect to database");
 ?>
